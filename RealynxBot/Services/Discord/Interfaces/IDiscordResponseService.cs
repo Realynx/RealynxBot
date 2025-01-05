5f37782cc70165f71c68f691fa3d511d835c387a
ScrubN <72096833+ScrubN@users.noreply.github.com>
2025-01-04 20:08:23 -0500

    Chunk messages on lines instead of chars

--- RealynxBot/Services/Discord/Interfaces/IDiscordResponseService.cs
+++ RealynxBot/Services/Discord/Interfaces/IDiscordResponseService.cs
@@ -1,5 +1,6 @@
 ﻿namespace RealynxBot.Services.Discord.Interfaces {
     public interface IDiscordResponseService {
+        IEnumerable<string> ChunkMessageToLines(string message);
         Task<bool> ChunkMessage(string largeMessage, Func<string, Task> followupAction);
     }
 }
